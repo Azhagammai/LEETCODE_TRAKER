ec728d19732f627ac55a3e4bbe7419c4cffe9183
Azhagammai <145277626+Azhagammai@users.noreply.github.com>
2025-07-14 04:42:37 +0545

    Time: 1 ms (99.67%) | Memory: 45.1 MB (78.20%) - LeetSync

--- 412-fizz-buzz/fizz-buzz.java
+++ 412-fizz-buzz/fizz-buzz.java
@@ -3,12 +3,11 @@ class Solution {
         ArrayList<String> list=new ArrayList<>();
   
         for(int i=1;i<=n;i++){
-            String str=""; 
-            str+=i;
+            
             if(i%3==0&&i%5==0)list.add("FizzBuzz");
             else if(i%3==0)list.add("Fizz");
             else if(i%5==0)list.add("Buzz");
-            else list.add(str);
+            else list.add(i+"");
         }
         return list;
         
